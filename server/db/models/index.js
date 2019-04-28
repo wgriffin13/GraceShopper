31fbcbf44be33c9d74d6e02368d8a88e1f4f0f1c
Ruby Barry <rubypbarry@gmail.com>
2019-04-28 18:38:04 -0400

    created user model and authenification routes

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,10 +1,13 @@
 const Product = require('./product');
 const Category = require('./category');
+const User = require('./User')
 
 Category.hasMany(Product);
 Product.belongsTo(Category);
 
+
 module.exports = {
   Product,
   Category,
+  User
 };
